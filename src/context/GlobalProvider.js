f9421776e71b762fdbac0beafca69cc979d1e495
Lucas Rodrigues <rodrigges@iMac-Pro.local>
2022-09-23 16:46:03 -0300

    adicionando fetch

--- src/context/GlobalProvider.js
+++ src/context/GlobalProvider.js
@@ -1,14 +1,26 @@
-import React, { createContext, useMemo, useState } from 'react';
+import React, { createContext, useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const GlobalContext = createContext();
 
+const CATEGORIES_URL = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+
 export function GlobalProvider({ children }) {
-  const [CONSTANTE, SETCONTANTE] = useState(null); // pra editar
+  const [mealCategories, setMealCaegories] = useState([]);
+
+  useEffect(() => {
+    fetch(CATEGORIES_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMealCaegories(data.meals
+          .reduce((acc, curr) => [...acc, curr.strCategory], []));
+      });
+  }, []);
 
   const context = useMemo(() => ({
-    CONSTANTE,
-    SETCONTANTE,
+    mealCategories,
+    setMealCaegories,
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }), []);
 
