87473e5cefef5ac1389573b31ca2fc9ddb47880b
Renato Dourad0 <99191410+RenatoDourad0@users.noreply.github.com>
2022-09-29 11:38:43 -0300

    commit pre pull

--- src/tests/FavoriteButton.test.js
+++ src/tests/FavoriteButton.test.js
@@ -4,36 +4,58 @@ import fetchMock from 'fetch-mock';
 import renderWithRouter from './helpers/renderWithRouter';
 import mealCategories from '../../cypress/mocks/mealCategories';
 import goatMeals from '../../cypress/mocks/goatMeals';
-import ginDrinks from '../../cypress/mocks/ginDrinks';
 import meals from '../../cypress/mocks/meals';
 import ordinaryDrinks from '../../cypress/mocks/ordinaryDrinks';
-import drinkCategories from '../../cypress/mocks/drinkCategories';
+import oneDrink from '../../cypress/mocks/oneDrink';
 import App from '../App';
 
 const mealCategoriesURL = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
-let currHistory;
-
-describe('Testa recipieDetails na rota meals', () => {
+describe('Testa recipieDetails', () => {
   beforeEach(async () => {
+  });
+  afterEach(() => {
+    fetchMock.restore();
+    jest.clearAllMocks();
+  });
+  test('se é possivel clicar no favoriteButton e se muda de cor na rota meals', async () => {
     fetchMock.get(mealCategoriesURL, mealCategories);
     fetchMock.get('https://www.themealdb.com/api/json/v1/1/search.php?s=', meals);
     fetchMock.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52968', goatMeals);
+    fetchMock.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=', ordinaryDrinks);
 
-    const { history } = renderWithRouter(<App />, ['meals/52968']);
-    currHistory = history;
+    const { history } = renderWithRouter(<App />, ['/meals/52968']);
 
     expect(fetchMock.called()).toBeTruthy();
 
-    await waitFor(() => expect(currHistory.location.pathname).toBe('meals/52968'));
+    await waitFor(() => expect(history.location.pathname).toBe('/meals/52968'));
+
+    const favBtn = screen.getByTestId('favorite-btn');
+
+    expect(favBtn).toHaveAttribute('src');
+    console.log(favBtn.child);
+    expect(favBtn.child.memoizedProps.src).toBe('whiteHeartIcon.svg');
+    userEvent.click(favBtn);
+    expect(favBtn.child.memoizedProps.src).toBe('blackHeartIcon.svg');
   });
-  test('se é possivel clicar no favoriteButton e se muda de cor', () => {
-    // const favBtn = screen.getByTestId('favorite-btn');
 
-    // expect(favBtn).toHaveAttribute('src');
-    // expect(favBtn.src).toBe('/static/media/whiteHeartIcon.2b822118952dc5140129c6349fcd0472.svg');
+  test('se é possivel clicar no favoriteButton e se muda de cor na rota drinks', async () => {
+    fetchMock.get(mealCategoriesURL, mealCategories);
+    fetchMock.get('https://www.themealdb.com/api/json/v1/1/search.php?s=', meals);
+    fetchMock.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=178319', oneDrink);
 
-    // userEvent.click(favBtn);
-    // expect(favBtn.src).toBe('/static/media/blackHeartIcon.083cb006913d197c95857ebfa2161db7.svg');
+    const { history } = renderWithRouter(<App />, ['/drinks/178319']);
+
+    expect(fetchMock.called()).toBeTruthy();
+
+    await waitFor(() => expect(history.location.pathname).toBe('/drinks/178319'));
+
+    // await waitFor(() => {
+    //   const favBtn = screen.getByTestId('favorite-btn');
+    //   expect(favBtn).toHaveAttribute('src');
+    //   expect(favBtn.src).toBe('/static/media/whiteHeartIcon.2b822118952dc5140129c6349fcd0472.svg');
+    //   userEvent.click(favBtn);
+    //   expect(favBtn.src).toBe('/static/media/blackHeartIcon.083cb006913d197c95857ebfa2161db7.svg');
+    // });
   });
 });
